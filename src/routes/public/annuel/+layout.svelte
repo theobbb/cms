d185a8942f7fe69f96410151ae84e97d65fd240c
Theo <theobbb64@gmail.com>
2026-05-01 13:40:59 -0400

    h

--- src/routes/public/annuel/+layout.svelte
+++ src/routes/public/annuel/+layout.svelte
@@ -10,6 +10,13 @@
 	init_pocketbase(apps.annuel.pocketbase.url, '');
 </script>
 
+<div class="absolute top-1.5 left-3.5">
+	<a
+		href="https://player.mux.com/OKPOFjumoxyrC87Q9cixuRK55woKEY301qZfPCqCJk2w?metadata-video-title=2026-04-28_19-17-27&video-title=2026-04-28_19-17-27"
+		target="_blank"
+		class="text-link">vidéo explicative →</a
+	>
+</div>
 <div class="mx-auto my-12 mb-36 max-w-2xl space-y-12 px-4">
 	<Header />
 	{@render children()}
