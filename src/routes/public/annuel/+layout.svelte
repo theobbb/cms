196908b74fdffc19e871d634ad6711aad09823d0
Theo <theobbb64@gmail.com>
2026-05-04 08:48:14 -0400

    g

--- src/routes/public/annuel/+layout.svelte
+++ src/routes/public/annuel/+layout.svelte
@@ -10,12 +10,23 @@
 	init_pocketbase(apps.annuel.pocketbase.url, '');
 </script>
 
-<div class="absolute top-1.5 left-3.5">
+<div class="absolute- mt-1.5 mb-24 px-4">
 	<a
 		href="https://player.mux.com/OKPOFjumoxyrC87Q9cixuRK55woKEY301qZfPCqCJk2w?metadata-video-title=2026-04-28_19-17-27&video-title=2026-04-28_19-17-27"
 		target="_blank"
 		class="text-link">vidéo explicative →</a
 	>
+	<div>
+		<div>Ce système (en évolution) contient des bugs :(</div>
+
+		<div>
+			Pour toutes questions ou si vous n’arrivez pas à ajouter vos images / vidéos, contactez :
+			theobbb64@gmail.com / 4383898908
+		</div>
+		<div>
+			Vous pouvez directement envoyer vos fichiers à ce courriel avec les infos nécessaires.
+		</div>
+	</div>
 </div>
 <div class="mx-auto my-12 mb-36 max-w-2xl space-y-12 px-4">
 	<Header />
